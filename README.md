e2177b389c4e84ac2452cace7b3fca94bf53d182
Bartłomiej 'PokerFacowaty' Nowak <pokerfacowaty.bn@gmail.com>
2025-03-23 22:43:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# kobookmarks
+
+WIP (including docs), made this to export markups from my Kobo Sage. This is a personal tool, but if a new feature would solve your problem, contact me or open an issue and I'll see what I can do.
---
+++ kobookmarks.py
@@ -0,0 +1,88 @@
+import sqlite3
+from pathlib import Path
+import subprocess
+import shutil
+import pymupdf
+import sys
+
+
+def main():
+    backup_dir = Path('/home/pokerfacowaty/kobo backup 23.03.2025')
+    dot_kobo_dir = backup_dir / '.kobo'
+    db_file = dot_kobo_dir / 'KoboReader.sqlite'
+    markups_folder = dot_kobo_dir / 'markups'
+    combined_markups_foder = Path(__file__).parent.resolve() / 'markups_combined'
+    last_update = '2025-03-01',
+
+    # Getting non-pdf markups
+    connection = sqlite3.connect(db_file)
+    # connection.text_factory = lambda b: b.decode(errors='ignore')
+    cur = connection.cursor()
+    response = cur.execute('SELECT BookmarkID, VolumeID FROM Bookmark WHERE Type = "markup" AND DateModified > ?', last_update)
+                           # + 'WHERE Type = "markup"'
+                           # + 'AND DateModifed > "?"', last_update)
+    non_pdf_bookmark_data = response.fetchall()
+    existing = 0
+
+    book_titles = {}
+    for i, _ in enumerate(non_pdf_bookmark_data):
+        # For testing
+        if (sys.argv[1] == 'skipnonpdf'):
+            break
+        bookmark_id = non_pdf_bookmark_data[i][0]
+        book_id = non_pdf_bookmark_data[i][1],
+
+        if book_titles.get(book_id, False):
+            book_title = book_titles[book_id]
+        else:
+            response = cur.execute('SELECT BookTitle FROM content'
+                                   + 'WHERE BookId = ? LIMIT 1', book_id)
+            book_title = response.fetchone()[0]
+            book_title = book_title.replace('/', '_')
+            book_titles[book_id] = book_title
+
+        # Skipping getting the author for now
+        svg_path = markups_folder / (bookmark_id + '.svg')
+        jpg_path = markups_folder / (bookmark_id + '.jpg')
+
+        book_dir = book_title
+        markup_filename = bookmark_id + '.jpg'
+
+        final_markup_dir = combined_markups_foder / book_dir
+        final_markup_path = final_markup_dir / markup_filename
+
+        if final_markup_path.exists():
+            print(f'[LOG] File exists: {markup_filename}, skipping...')
+            existing += 1
+            continue
+        Path.mkdir(final_markup_dir, exist_ok=True)
+        ffmpeg_args = [shutil.which('ffmpeg'), '-hide_banner', '-loglevel',
+                       'error', '-y', '-i', jpg_path, '-i', svg_path,
+                       '-filter_complex', "'overlay'", final_markup_path]
+        subprocess.Popen(ffmpeg_args)
+    print(existing)
+
+    pdf_paths = []
+    for dirpath, _, filenames in backup_dir.walk():
+        for fname in filenames:
+            if fname.endswith('pdf'):
+                pdf_paths.append(dirpath / fname)
+    print(pdf_paths)
+
+    for pdf_path in pdf_paths:
+        doc = pymupdf.open(pdf_path)
+        zoom_x = 3.0
+        zoom_y = 3.0
+        mat = pymupdf.Matrix(zoom_x, zoom_y)
+
+        for pageNum, page in enumerate(doc):
+            if page.first_annot:
+                pix = page.get_pixmap(matrix=mat)
+                final_markup_dir = combined_markups_foder / pdf_path.stem
+                print(final_markup_dir)
+                Path.mkdir(final_markup_dir, exist_ok=True)
+                pix.save(final_markup_dir / f'{pageNum + 1}.jpg')
+
+
+if __name__ == '__main__':
+    main()
