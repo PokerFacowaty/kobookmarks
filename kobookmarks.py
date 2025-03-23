eec81943daee26f53210e8fa98501a0b96da407b
Bartłomiej 'PokerFacowaty' Nowak <pokerfacowaty.bn@gmail.com>
2025-03-23 23:37:08 +0100

    Moved getting non-pdf markups and pdf ink annotations that are there so far into their separate functions

--- kobookmarks.py
+++ kobookmarks.py
@@ -14,13 +14,17 @@ def main():
     combined_markups_foder = Path(__file__).parent.resolve() / 'markups_combined'
     last_update = '2025-03-01',
 
-    # Getting non-pdf markups
+    get_nonpdf_markups(db_file, last_update, markups_folder,
+                       combined_markups_foder)
+    get_pdf_annotations(backup_dir, combined_markups_foder)
+
+
+def get_nonpdf_markups(db_file, last_update, markups_folder,
+                       combined_markups_folder):
     connection = sqlite3.connect(db_file)
     # connection.text_factory = lambda b: b.decode(errors='ignore')
     cur = connection.cursor()
     response = cur.execute('SELECT BookmarkID, VolumeID FROM Bookmark WHERE Type = "markup" AND DateModified > ?', last_update)
-                           # + 'WHERE Type = "markup"'
-                           # + 'AND DateModifed > "?"', last_update)
     non_pdf_bookmark_data = response.fetchall()
     existing = 0
 
@@ -48,7 +52,7 @@ def main():
         book_dir = book_title
         markup_filename = bookmark_id + '.jpg'
 
-        final_markup_dir = combined_markups_foder / book_dir
+        final_markup_dir = combined_markups_folder / book_dir
         final_markup_path = final_markup_dir / markup_filename
 
         if final_markup_path.exists():
@@ -62,6 +66,8 @@ def main():
         subprocess.Popen(ffmpeg_args)
     print(existing)
 
+
+def get_pdf_ink_annotations(backup_dir, combined_markups_folder):
     pdf_paths = []
     for dirpath, _, filenames in backup_dir.walk():
         for fname in filenames:
@@ -78,7 +84,7 @@ def main():
         for pageNum, page in enumerate(doc):
             if page.first_annot:
                 pix = page.get_pixmap(matrix=mat)
-                final_markup_dir = combined_markups_foder / pdf_path.stem
+                final_markup_dir = combined_markups_folder / pdf_path.stem
                 print(final_markup_dir)
                 Path.mkdir(final_markup_dir, exist_ok=True)
                 pix.save(final_markup_dir / f'{pageNum + 1}.jpg')
